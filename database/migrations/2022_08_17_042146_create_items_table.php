ebf9193ba6379ef542837bbb6c9e161c0cef7748
EsmerellEzra03 <esmerellezraensil@gmail.com>
2022-08-17 12:44:17 +0800

    create item table

---
+++ database/migrations/2022_08_17_042146_create_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('quantity');
+            $table->unsignedInteger('condition_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('condition_id')->references('id')->on('conditions')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+};
