aac45af255613112186c4030f90bd7e8136bb7cc
EsmerellEzra03 <esmerellezraensil@gmail.com>
2022-08-18 15:00:50 +0800

    create route for every function

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,17 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
 
 Route::get('/condition/index', [App\Http\Controllers\ConditionController::class, 'index'])->name('condition:index');
 
 Route::get('/condition/create', [App\Http\Controllers\ConditionController::class, 'create'])->name('condition:create');
+
+Route::post('/condition/store', [App\Http\Controllers\ConditionController::class, 'store'])->name('condition:store');
+
+Route::get('/condition/show/{condition}', [App\Http\Controllers\ConditionController::class, 'show'])->name('condition:show');
+
+Route::post('/condition/edit/{condition}', [App\Http\Controllers\ConditionController::class, 'edit'])->name('condition:edit');
+
+Route::get('/condition/destroy/{condition}', [App\Http\Controllers\ConditionController::class, 'destroy'])->name('condition:destroy');
+
+require __DIR__.'/auth.php';
