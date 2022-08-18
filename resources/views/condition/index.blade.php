32a0f41330266d177c3a20a011629a9551a78709
EsmerellEzra03 <esmerellezraensil@gmail.com>
2022-08-18 14:58:18 +0800

    create table

--- resources/views/condition/index.blade.php
+++ resources/views/condition/index.blade.php
@@ -9,14 +9,61 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    ayam
-                    <a href="{{ url('/dashboard')}}" type="button" class="border border-gray-700 bg-gray-700 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline">
-                        Go to Dashboard
-                    </a>
+                    Item list
+                    
                     <a href="{{ route('condition:create')}}" type="button" class="border border-gray-700 bg-gray-700 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline">
                         Create Condition
                     </a>
 
+                    <table class="min-w-full">
+                        <thead>
+                            <tr>
+                                <th
+                                    class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                                    ID
+                                </th>
+                                <th
+                                    class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                                    Name
+                                </th>
+                                <th
+                                class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                                Actions
+                            </th>
+                            </tr>
+                        </thead>
+
+                        <tbody class="bg-white">
+                           
+                        @foreach($conditions as $condition)       
+
+                            <tr>
+                                <td
+                                    class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
+                                    <span> {{ $condition->id }}</span>
+                                </td>
+
+                                <td
+                                class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
+                                <span> {{ $condition->name }}</span>
+                                </td>
+
+                                <td>
+                                
+                                <a href="{{ route('condition:show',$condition) }} " type="button" class="border border-gray-700 bg-gray-700 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline">
+                                    Edit
+                                </a>
+
+                                <a href=" {{ route('condition:destroy', $condition) }}" type="button" class="border border-gray-700 bg-red-700 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline">
+                                    Delete
+                                </a>
+                             
+                                </td>
+                                
+                            </tr>
+                         @endforeach  
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
