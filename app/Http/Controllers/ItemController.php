cd10a1044d2339b46bc164762431363a32273305
EsmerellEzra03 <esmerellezraensil@gmail.com>
2022-08-19 13:13:46 +0800

    create ItemController

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items=Item::all();
+        return view('item.index',compact('items'));
+    }
+    
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('item.item');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //declare model
+        $item = new Item;
+        $item->name=$request->name;
+        $item->quantity=$request->quantity;
+        $item->save();
+
+        return redirect()->route('item:index')->with('message', 'Item Added Successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Item $item)
+    {
+        //create editform
+        return view('item.show',compact('item'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Request $request,Item $item)
+    {
+        //Update 
+    
+        $item->name=$request->name;
+        $item->quantity=$request->quantity;
+        $item->save();
+
+        return redirect()->route('item:index')->with('message', 'Item Updated Successfully');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Item $item)
+    {
+        //To delete 
+        $item->delete();
+
+        return redirect()->route('item:index')->with('message', 'Item Deleted');
+
+    }
+}
