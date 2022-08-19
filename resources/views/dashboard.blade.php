84f47c718b9737aa06917a3f36890b4598ed733f
EsmerellEzra03 <esmerellezraensil@gmail.com>
2022-08-19 11:59:46 +0800

    added button to go item module

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,10 +9,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    Hello !
+                    Hello ! 
                     <a href="{{ route('condition:index')}}" type="button" class="border border-gray-700 bg-gray-700 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline">
                      Go to Condition
                     </a>
+                    <a href="{{ route('item:index')}}" type="button" class="border border-gray-700 bg-gray-700 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline">
+                        Go to Items
+                       </a>
 
                 </div>
             </div>
